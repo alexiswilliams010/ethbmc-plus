e018169d78257c9301c2d18afb87afafa4a5f4fd
alexiswilliams010 <204508632+alexiswilliams010@users.noreply.github.com>
2025-05-21 20:31:49 -0400

    Wrap error to avoid panics

--- forge-runner/bin/main.rs
+++ forge-runner/bin/main.rs
@@ -4,5 +4,8 @@ use forge_runner::custom_tester::CustomTestArgs;
 #[tokio::main]
 async fn main() {
     let args = CustomTestArgs::parse();
-    args.run().await.unwrap();
+    if let Err(e) = args.run().await {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
